708f4c700e16aad16b1ee12dc374de9a7f8eff17
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-07 15:23:47 +0300

    save data

--- Blog/src/Controller/ArticleController.php
+++ Blog/src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -17,4 +18,22 @@ class ArticleController extends Controller
     {
         return $this->render('articles/index.html.twig');
     }
+
+    /**
+     * @Route("/article/create")
+     */
+     public function save()
+     {
+         $entityManager = $this->getDoctrine()->getManager();
+
+         $article = new Article();
+         $article->setTitle("Article one");
+         $article->setBody('body one');
+
+         $entityManager->persist($article);
+
+         $entityManager->flush();
+
+         return new Response('save new article of id ' . $article->getId());
+     }
 }
