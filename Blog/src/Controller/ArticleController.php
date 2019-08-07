3587bcdb70c6fe7ec42fb3032d43aa205fc3401d
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-07 15:35:52 +0300

    show

--- Blog/src/Controller/ArticleController.php
+++ Blog/src/Controller/ArticleController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ArticleController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      */
     public function index()
@@ -26,11 +26,16 @@ class ArticleController extends Controller
     }
 
     /**
-     * @Route("/article/{id}")
-     * @Method({"GET"})
+     * @Route("/article/{id}", name="article_show")
      */
-     public function show()
+     public function show($id)
      {
-         
+         $article = $this->getDoctrine()
+                         ->getRepository(Article::class)
+                         ->find($id);
+
+        return $this->render('articles/show.html.twig', [
+            'article' => $article
+        ]);
      }
 }
