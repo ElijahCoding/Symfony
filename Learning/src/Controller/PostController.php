215ef97dfb6c42213e8cf02c2658c3c79a25c181
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-08 16:31:59 +0300

    handle with entity manager, what a pain in ass

--- Learning/src/Controller/PostController.php
+++ Learning/src/Controller/PostController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/post", name="post.")
+ */
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/", name="index")
      */
     public function index()
     {
@@ -19,10 +23,19 @@ class PostController extends AbstractController
     }
 
     /**
-     * @Route("/post/create", name="post")
+     * @Route("/create", name="create")
      */
      public function create(Request $request)
      {
-         
+         $post = new Post();
+
+         $post->setTitle('Title one');
+
+         // entity manager
+         $em = $this->getDoctrine()->getManager();
+         $em->persist($post);
+         $em->flush();
+
+         return new Response('Post created');
      }
 }
