e7cc25505d1d2924c68332b6516124f211772af5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-09 13:54:16 +0300

    register form , handing request

--- Learning/src/Controller/RegistrationController.php
+++ Learning/src/Controller/RegistrationController.php
@@ -4,23 +4,38 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Form\Extension\Core\Type\{RepeatedType, PasswordType};
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\{RepeatedType, PasswordType, SubmitType};
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="register")
      */
-    public function register()
+    public function register(Request $request)
     {
         $form = $this->createFormBuilder()
                      ->add('username')
                      ->add('password', RepeatedType::class, [
                          'type' => PasswordType::class,
-                     ]);
+                         'first_options'  => ['label' => 'Password'],
+                         'second_options' => ['label' => 'Repeat Password'],
+                     ])
+                     ->add('register', SubmitType::class, [
+                         'attr' => [
+                             'class' => 'btn btn-success float-right'
+                         ]
+                     ])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->submitted()) {
+            
+        }
 
         return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'form' => $form->createView()
         ]);
     }
 }
