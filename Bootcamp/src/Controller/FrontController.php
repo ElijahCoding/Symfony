80c5f279c3193bdd39f14fc5bd9f40ec109c9dc2
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-12 20:16:33 +0300

    all templates ready, building admin pages

--- Bootcamp/src/Controller/FrontController.php
+++ Bootcamp/src/Controller/FrontController.php
@@ -8,13 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="front")
+     * @Route("/", name="main_page")
      */
     public function index()
     {
-        return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
-        ]);
+        return $this->render('front/index.html.twig');
     }
 
     /**
@@ -31,5 +29,37 @@ class FrontController extends AbstractController
     public function videoDetails()
     {
         return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResults()
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
     }
 }
