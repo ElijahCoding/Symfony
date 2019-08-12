834cccd211553b3bb163a158b6a5aa83fd8ae360
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-12 19:11:45 +0300

    Task Entity

--- TodoCrud/src/Controller/TodoListController.php
+++ TodoCrud/src/Controller/TodoListController.php
@@ -14,4 +14,28 @@ class TodoListController extends AbstractController
     {
         return $this->render('index.html.twig');
     }
+
+    /**
+     * @Route("/todo/create", name="create_task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('to do: create a new task!');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch_status")
+     */
+    public function switchStatus($id)
+    {
+          exit('to do: switch status of the task!'. $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="task_delete")
+     */
+    public function delete($id)
+    {
+        exit('to do: delete a task with the id of !'. $id);
+    }
 }
