b2bc3333af456f13138408d57f665696c2ac74f3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-12 20:08:22 +0300

    video-list and video-detail

--- Bootcamp/src/Controller/FrontController.php
+++ Bootcamp/src/Controller/FrontController.php
@@ -16,4 +16,20 @@ class FrontController extends AbstractController
             'controller_name' => 'FrontController',
         ]);
     }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
 }
