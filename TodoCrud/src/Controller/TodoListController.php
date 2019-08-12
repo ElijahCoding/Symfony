7a99288ac5e24fac4d90466b7d9d5912f28bc18f
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-12 18:09:41 +0300

    all pkgs built

---
+++ TodoCrud/src/Controller/TodoListController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TodoListController extends AbstractController
+{
+    /**
+     * @Route("/todo/list", name="todo_list")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/TodoListController.php',
+        ]);
+    }
+}
