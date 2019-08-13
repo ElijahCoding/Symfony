743ac8e4daa885c5665c6453b509439a6593d743
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-13 21:03:35 +0300

    relationship built

--- ApiPlatform/src/Entity/Task.php
+++ ApiPlatform/src/Entity/Task.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -25,6 +27,21 @@ class Task
      * @ORM\Column(type="boolean")
      */
     private $isComplete;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\TaskList", inversedBy="tasks")
+     */
+    private $list;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Note", mappedBy="task")
+     */
+    private $notes;
+
+    public function __construct()
+    {
+        $this->notes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -51,6 +68,49 @@ class Task
     public function setIsComplete(bool $isComplete): self
     {
         $this->isComplete = $isComplete;
+
+        return $this;
+    }
+
+    public function getList(): ?TaskList
+    {
+        return $this->list;
+    }
+
+    public function setList(?TaskList $list): self
+    {
+        $this->list = $list;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Note[]
+     */
+    public function getNotes(): Collection
+    {
+        return $this->notes;
+    }
+
+    public function addNote(Note $note): self
+    {
+        if (!$this->notes->contains($note)) {
+            $this->notes[] = $note;
+            $note->setTask($this);
+        }
+
+        return $this;
+    }
+
+    public function removeNote(Note $note): self
+    {
+        if ($this->notes->contains($note)) {
+            $this->notes->removeElement($note);
+            // set the owning side to null (unless already changed)
+            if ($note->getTask() === $this) {
+                $note->setTask(null);
+            }
+        }
 
         return $this;
     }
--- ApiPlatform/src/Entity/TaskList.php
+++ ApiPlatform/src/Entity/TaskList.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -30,6 +32,21 @@ class TaskList
      * @ORM\Column(type="string", length=255)
      */
     private $backgroundPath;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="list")
+     */
+    private $tasks;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Task", inversedBy="notes")
+     */
+    private $task;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -68,6 +85,49 @@ class TaskList
     public function setBackgroundPath(string $backgroundPath): self
     {
         $this->backgroundPath = $backgroundPath;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Product[]
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Product $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setList($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Product $task): self
+    {
+        if ($this->tasks->contains($task)) {
+            $this->tasks->removeElement($task);
+            // set the owning side to null (unless already changed)
+            if ($task->getList() === $this) {
+                $task->setList(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getTask(): ?Task
+    {
+        return $this->task;
+    }
+
+    public function setTask(?Task $task): self
+    {
+        $this->task = $task;
 
         return $this;
     }
