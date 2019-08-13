6d035c265f1ed964a905f91b4bbc75c12afe5618
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-13 17:20:42 +0300

    debugging

--- Bootcamp/src/Controller/FrontController.php
+++ Bootcamp/src/Controller/FrontController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Utils\CategoryTreeFrontPage;
 
 class FrontController extends AbstractController
 {
@@ -19,8 +20,9 @@ class FrontController extends AbstractController
     /**
      * @Route("/video-list/category/{categoryname},{id}", name="video_list")
      */
-    public function videoList()
+    public function videoList($id, CategoryTreeFrontPage $categories)
     {
+        // dump(CategoryTreeFrontPage);
         return $this->render('front/video_list.html.twig');
     }
 
