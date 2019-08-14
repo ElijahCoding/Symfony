2415eba2d4cdf73ec49d7f2b4f68f01ac8075f32
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-14 12:56:45 +0300

    first endpoint

--- ApiPlatform/src/Controller/ListController.php
+++ ApiPlatform/src/Controller/ListController.php
@@ -2,13 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\TaskListRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 class ListController extends AbstractFOSRestController
 {
+    /**
+     * @var TaskListRepository
+     */
+    private $taskListRepository;
+
+    public function __construct(TaskListRepository $taskListRepository)
+    {
+        $this->taskListRepository = $taskListRepository;
+    }
+
     public function getListsAction()
     {
-
+        return $this->taskListRepository->findAll();
     }
 
     public function postListsAction()
