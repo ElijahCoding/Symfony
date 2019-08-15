6e298de7c47a7b8a3773421b5e3c567c1e8644db
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-15 17:23:05 +0300

    refactor to repository

--- Relation/src/Entity/Article.php
+++ Relation/src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\Criteria;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
@@ -185,9 +186,7 @@ class Article
      */
     public function getNonDeletedComments(): Collection
     {
-        $criteria = Criteria::create()->andWhere(Criteria::expr()
-                    ->eq('isDeleted', false))
-                    ->orderBy(['createdAt' => 'DESC']);
+        $criteria = ArticleRepository::createNonDeletedCriteria();
 
         return $this->comments->matching($criteria);
     }
