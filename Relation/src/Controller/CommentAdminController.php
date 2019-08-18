eaa810158f4c040425f28e99b517beaa6abaf5a6
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-18 23:43:32 +0300

    pagination

--- Relation/src/Controller/CommentAdminController.php
+++ Relation/src/Controller/CommentAdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,14 +13,25 @@ class CommentAdminController extends Controller
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(CommentRepository $repository, Request $request)
+    public function index(
+        CommentRepository $repository,
+        Request $request,
+        PaginatorInterface $paginator
+    )
     {
         $q = $request->query->get('q');
+        $queryBuilder = $repository->getWithSearchQueryBuilder($q);
 
-        $comments = $repository->findAllBySearch($q);
+        // $comments = $repository->findAllBySearch($q);
+
+        $pagination = $paginator->paginate(
+            $queryBuilder, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
 
         return $this->render('comment_admin/index.html.twig', [
-            'comments' => $comments
+            'pagination' => $pagination
         ]);
     }
 }
