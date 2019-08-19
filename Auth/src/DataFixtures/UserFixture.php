619862c856638787ce18aa818b857049356dac5a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-19 23:41:34 +0300

    faker user data

--- Auth/src/DataFixtures/UserFixture.php
+++ Auth/src/DataFixtures/UserFixture.php
@@ -4,12 +4,30 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
-        
+        $this->createMany(10, 'main_users', function($i) use ($manager) {
+            $user = new User();
+            $user->setEmail(sprintf('spacebar%d@example.com', $i));
+            $user->setFirstName($this->faker->firstName);
+
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'hellojava'
+            ));
+
+
+            return $user;
+        });
 
         $manager->flush();
     }
