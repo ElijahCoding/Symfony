5e8683d307f8e92e33c6e4371f09c2ab191698ac
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-20 17:44:36 +0300

    access control in controller

--- Auth/src/Controller/CommentAdminController.php
+++ Auth/src/Controller/CommentAdminController.php
@@ -7,7 +7,11 @@ use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @IsGranted("ROLE_ADMIN")
+ */
 class CommentAdminController extends Controller
 {
     /**
@@ -15,6 +19,8 @@ class CommentAdminController extends Controller
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
+        // $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $q = $request->query->get('q');
 
         $queryBuilder = $repository->getWithSearchQueryBuilder($q);
