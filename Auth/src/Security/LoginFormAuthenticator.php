0b8a7b85c318af3d67b8f0b9e7b8be40531b170b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-20 14:06:44 +0300

    redirect

--- Auth/src/Security/LoginFormAuthenticator.php
+++ Auth/src/Security/LoginFormAuthenticator.php
@@ -3,13 +3,26 @@
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Routing\RouterInterface;
+use App\Repository\UserRepository;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
+    private $userRepository;
+
+    private $router;
+
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
     public function supports(Request $request)
     {
         return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
@@ -17,7 +30,6 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function getCredentials(Request $request)
     {
-        // if supports returns true, then this function will be called
         return [
             'email' => $request->request->get('email'),
             'password' => $request->request->get('password'),
@@ -27,17 +39,17 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        dd($credentials);
+        return $this->userRepository->findOneBy(['email' => $credentials['email']]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-
+        return true;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-
+        return new RedirectResponse($this->router->generate('app_homepage'));
     }
 
     protected function getLoginUrl()
