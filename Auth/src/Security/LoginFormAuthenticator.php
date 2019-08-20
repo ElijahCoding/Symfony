263a829f6c3f3b875be419c189f8eff978e0450e
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-20 13:52:45 +0300

    digging LoginFormAuthenticator.php

--- Auth/src/Security/LoginFormAuthenticator.php
+++ Auth/src/Security/LoginFormAuthenticator.php
@@ -12,17 +12,22 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
     public function supports(Request $request)
     {
-        die('working');
+        return $request->attributes->get('_route') === 'app_login' && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
     {
-
+        // if supports returns true, then this function will be called
+        return [
+            'email' => $request->request->get('email'),
+            'password' => $request->request->get('password'),
+            'csrf_token' => $request->request->get('_csrf_token'),
+        ];
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-
+        dd($credentials);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
