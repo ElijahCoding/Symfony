27d23355608ac973ce46aedb6df604906c44592b
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-26 21:04:05 +0300

    unmapped fields

--- Form/src/Form/UserRegistrationFormType.php
+++ Form/src/Form/UserRegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserRegistrationFormType extends AbstractType
 {
@@ -13,7 +14,9 @@ class UserRegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('password')
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false
+            ])
         ;
     }
 
