305b1b106ef3671b85de643b55c02ce52072d692
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-26 16:32:27 +0300

    form type

--- Form/src/Form/ArticleFormType.php
+++ Form/src/Form/ArticleFormType.php
@@ -6,12 +6,15 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ArticleFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')
+        $builder->add('title', TextType::class, [
+            'help' => 'Choose something catchy!'
+        ])
                 ->add('content');
     }
 
