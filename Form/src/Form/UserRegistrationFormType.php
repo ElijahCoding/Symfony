bbc10cf2d0bd04881ba304505e277dd9a5445a7c
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-28 22:57:50 +0300

    agreedTermsAt checkbox field

--- Form/src/Form/UserRegistrationFormType.php
+++ Form/src/Form/UserRegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Validator\Constraints\{NotBlank, Length};
 
 class UserRegistrationFormType extends AbstractType
@@ -28,6 +29,7 @@ class UserRegistrationFormType extends AbstractType
                     ])
                 ]
             ])
+            ->add('agreeTerms', CheckboxType::class)
         ;
     }
 
