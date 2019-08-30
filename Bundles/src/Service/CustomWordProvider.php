e4a209b8e7b1cd95e808e9f020adccc18d735be5
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-30 10:03:36 +0300

    built

---
+++ Bundles/src/Service/CustomWordProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+use KnpU\LoremIpsumBundle\WordProviderInterface;
+
+class CustomWordProvider implements WordProviderInterface
+{
+    public function getWordList(): array
+    {
+        return ['beach'];
+    }
+}
