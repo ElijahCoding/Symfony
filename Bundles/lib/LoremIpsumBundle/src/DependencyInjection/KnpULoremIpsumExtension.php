8cd35fd9acd7c14f8a94bede054491ddb3c748a1
ElijahCoding <cuiyeqing960904@gmail.com>
2019-08-30 09:57:46 +0300

    registering

--- Bundles/lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
+++ Bundles/lib/LoremIpsumBundle/src/DependencyInjection/KnpULoremIpsumExtension.php
@@ -2,13 +2,20 @@
 
 namespace KnpU\LoremIpsumBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class KnpULoremIpsumExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        var_dump('working');
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.xml');
     }
 }
