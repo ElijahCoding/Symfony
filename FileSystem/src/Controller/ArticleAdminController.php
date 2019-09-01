304ab058c4b46888bce1890c4ff8036dbc6f6e75
ElijahCoding <cuiyeqing960904@gmail.com>
2019-09-01 20:19:09 +0300

    upload files

--- FileSystem/src/Controller/ArticleAdminController.php
+++ FileSystem/src/Controller/ArticleAdminController.php
@@ -53,6 +53,19 @@ class ArticleAdminController extends BaseController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form['imageFile']->getData();
+
+            if ($uploadedFile) {
+                $destination = $this->getParameter('kernel.project_dir') . '/public/uploads/article_image';
+
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = Urlizer::urlize($originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension());
+
+                $uploadedFile->move($destination, $newFilename);
+
+                $article->setImageFilename($newFilename);
+            }
+
             $em->persist($article);
             $em->flush();
 
@@ -66,21 +79,6 @@ class ArticleAdminController extends BaseController
         return $this->render('article_admin/edit.html.twig', [
             'articleForm' => $form->createView()
         ]);
-    }
-
-    /**
-     * @Route("/admin/upload/test", name="upload_test")
-     */
-    public function temporaryUploadAction(Request $request)
-    {
-        $uploadedFile = $request->files->get('image');
-
-        $destination = $this->getParameter('kernel.project_dir') . '/public/uploads';
-
-        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $newFilename = Urlizer::urlize($originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension());
-        
-        $uploadedFile->move($destination, $newFilename);
     }
 
     /**
